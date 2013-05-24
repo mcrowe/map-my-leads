35cd4fbd3c742738e1b2314ee78f6ac3e820c02a
Mitch Crowe <crowe.mitch@gmail.com>
2013-05-24 11:29:07 -0700

    Add locations.

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -12,12 +12,32 @@ class Lead
 
   delegate :id, :created_at, :submitter_ip, to: :unbounce_lead
 
+  delegate :latitude, :longitude, :country_code, :country, to: :location
+
   def as_json(options = {})
     {
       id: id,
       createdAt: created_at,
-      submitterIp: submitter_ip
+      submitterIp: submitter_ip,
+      latitude: latitude,
+      longitude: longitude,
+      countryCode: country_code,
+      country: country
     }
   end
+
+  private
+
+    def location
+      @location ||= compute_location
+    end
+
+    def compute_location
+      Geocoder.search(submitter_ip).first || null_location
+    end
+
+    def null_location
+      OpenStruct.new(latitude: nil, longitude: nil, country_code: nil, country: nil)
+    end
 
 end
